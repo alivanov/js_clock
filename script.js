dcc56594cf5bf503c0fd12a5dd85440daaf529cb
alivanov <ivanov@intspirit.com>
2020-10-16 16:30:40 +0300

    make it live

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const hr = document.getElementById('hr');
 const mn = document.getElementById('mn');
 const sc = document.getElementById('sc');
 
-let now = new Date();
-let hh = now.getHours() * 30;
-let mm = now.getMinutes() * DEG;
-let ss = now.getSeconds() * DEG;
-
-hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-mn.style.transform = `rotateZ(${mm}deg)`;
-sc.style.transform = `rotateZ(${ss}deg)`;
+setInterval(() => {
+  let now = new Date();
+  let hh = now.getHours() * 30;
+  let mm = now.getMinutes() * DEG;
+  let ss = now.getSeconds() * DEG;
+  
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+})
